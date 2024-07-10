fa752ce8ef9297186061a40d56d53cb195e3e9fe
OsamaError <91324275+OsamaError@users.noreply.github.com>
2024-07-11 00:39:53 +0300

    q2

---
+++ q2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int i, num, sum=0;
+    float average;
+
+    for (i=1 ; i<=10 ; i++) {
+        printf("Enter number %d: \n"i);
+        scanf("%d", &num);
+        sum += num;
+    }
+
+    average = (float)sum/10.0;
+
+    printf("The sum of the numbers is: %d\n", sum);
+    printf("The average of the numbers is: %lf\n", average);
+
+    return 0;
+}
