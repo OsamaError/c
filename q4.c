8d185243a349dad6a2613cd13f2849d05643f464
OsamaError <91324275+OsamaError@users.noreply.github.com>
2024-07-11 16:47:39 +0300

    q4

--- q4.c
+++ q4.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    int answer;
+    printf("What is the result of 3 x 4?\n");
+
+    do {
+        scanf("%d", &answer);
+
+        if (answer == 12 ) {
+            printf("Thanks!\n");
+            break;
+        }
+        else {
+            printf("Try again.\n");
+        }
+    } while (1);
 
 
     return 0;
