2d54566efcf390f76598e3d2dc7a611179a4d013
OsamaError <91324275+OsamaError@users.noreply.github.com>
2024-07-11 16:29:58 +0300

    q3

--- q3.c
+++ q3.c
@@ -2,6 +2,16 @@
 
 int main(){
 
+    int i, num;
+    printf("Enter the number: \n");
+    scanf("%d", &num);
+
+    printf("Multiplication table of %d:\n", num);
+
+    for(i=1 ; i>=12 ; i++){
+        printf("%d x %d = %d \n", num, i, num*i);
+    }
+
 
     return 0;
 }
